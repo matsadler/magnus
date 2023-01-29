8052d9abdfb9eb8e2ef5283273cb9487bb4adf18
Mat Sadler <mat@sourcetagsandcodes.com>
2023-01-28 17:31:56 -0800

    doc RMatch methods

--- src/r_match.rs
+++ src/r_match.rs
@@ -40,16 +40,86 @@ impl RMatch {
         Self(NonZeroValue::new_unchecked(Value::new(val)))
     }
 
+    /// Returns whether the `n`th capture group is set.
+    ///
+    /// Returns `Some(true)` when there is an `n`th capture and it has a value.
+    /// Returns `Some(false)` when there is an `n`th capture but it is empty.
+    /// Returns `None` when there is no `n`th capture.
+    ///
+    /// This function is similar to [`nth_match`](Self::nth_match), but can be
+    /// used to avoid allocating a Ruby string if the value of the capture is
+    /// not needed.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use magnus::{backref_get, RRegexp};
+    /// # let _cleanup = unsafe { magnus::embed::init() };
+    ///
+    /// let regexp = RRegexp::new(".([a-z])([a-z]*)([0-9])?", Default::default()).unwrap();
+    /// regexp.reg_match("ex").unwrap();
+    /// let match_data = backref_get().unwrap();
+    /// // 0th group is the whole match
+    /// assert_eq!(match_data.nth_defined(0), Some(true));
+    /// // the `([a-z])` group
+    /// assert_eq!(match_data.nth_defined(1), Some(true));
+    /// // the `([a-z]*)` group
+    /// assert_eq!(match_data.nth_defined(2), Some(true));
+    /// // the `([0-9])?` group
+    /// assert_eq!(match_data.nth_defined(3), Some(false));
+    /// // no 4th group
+    /// assert_eq!(match_data.nth_defined(4), None);
+    /// ```
     pub fn nth_defined(self, n: isize) -> Option<bool> {
         let value = unsafe { Value::new(rb_reg_nth_defined(n as c_int, self.as_rb_value())) };
-        value.try_convert().unwrap() // conversion to Option<bool> in infallible
+        value.try_convert().unwrap() // conversion to Option<bool> is infallible
     }
 
+    /// Returns the string captured by the `n`th capture group.
+    ///
+    /// Returns `None` when there is no `n`th capture.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use magnus::{backref_get, RRegexp};
+    /// # let _cleanup = unsafe { magnus::embed::init() };
+    ///
+    /// let regexp = RRegexp::new(".([a-z])([a-z]*)([0-9])?", Default::default()).unwrap();
+    /// regexp.reg_match("ex").unwrap();
+    /// let match_data = backref_get().unwrap();
+    /// // 0th group is the whole match
+    /// assert_eq!(match_data.nth_match(0).map(|s| s.to_string().unwrap()), Some(String::from("ex")));
+    /// // the `([a-z])` group
+    /// assert_eq!(match_data.nth_match(1).map(|s| s.to_string().unwrap()), Some(String::from("x")));
+    /// // the `([a-z]*)` group
+    /// assert_eq!(match_data.nth_match(2).map(|s| s.to_string().unwrap()), Some(String::from("")));
+    /// // the `([0-9])?` group
+    /// assert_eq!(match_data.nth_match(3).map(|s| s.to_string().unwrap()), None);
+    /// // no 4th group
+    /// assert_eq!(match_data.nth_match(4).map(|s| s.to_string().unwrap()), None);
+    /// ```
     pub fn nth_match(self, n: isize) -> Option<RString> {
         let value = unsafe { Value::new(rb_reg_nth_match(n as c_int, self.as_rb_value())) };
         (!value.is_nil()).then(|| unsafe { RString::from_rb_value_unchecked(value.as_rb_value()) })
     }
 
+    /// Returns the index for the named capture group.
+    ///
+    /// Returns `Err` if there's is no named capture group with the given name.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use magnus::{backref_get, RRegexp};
+    /// # let _cleanup = unsafe { magnus::embed::init() };
+    ///
+    /// let regexp = RRegexp::new("Hello, (?<subject>.*)!", Default::default()).unwrap();
+    /// regexp.reg_match("Hello, World!").unwrap();
+    /// let match_data = backref_get().unwrap();
+    /// assert_eq!(match_data.backref_number("subject").unwrap(), 1);
+    /// assert!(match_data.backref_number("foo").is_err());
+    /// ```
     pub fn backref_number<T>(self, name: T) -> Result<usize, Error>
     where
         T: IntoRString,
@@ -63,20 +133,79 @@ impl RMatch {
         Ok(n)
     }
 
+    /// Returns the string matched.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use magnus::{backref_get, RRegexp};
+    /// # let _cleanup = unsafe { magnus::embed::init() };
+    ///
+    /// let regexp = RRegexp::new("b(.)r", Default::default()).unwrap();
+    /// regexp.reg_match("foo bar baz").unwrap();
+    ///
+    /// let match_data = backref_get().unwrap();
+    /// assert_eq!(match_data.matched().to_string().unwrap(), "bar");
+    /// ```
     pub fn matched(self) -> RString {
         unsafe { RString::from_rb_value_unchecked(rb_reg_last_match(self.as_rb_value())) }
     }
 
+    /// Returns the string before the segment matched.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use magnus::{backref_get, RRegexp};
+    /// # let _cleanup = unsafe { magnus::embed::init() };
+    ///
+    /// let regexp = RRegexp::new("b(.)r", Default::default()).unwrap();
+    /// regexp.reg_match("foo bar baz").unwrap();
+    ///
+    /// let match_data = backref_get().unwrap();
+    /// assert_eq!(match_data.pre().to_string().unwrap(), "foo ");
+    /// ```
     pub fn pre(self) -> RString {
         unsafe { RString::from_rb_value_unchecked(rb_reg_match_pre(self.as_rb_value())) }
     }
 
+    /// Returns the string after the segment matched.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use magnus::{backref_get, RRegexp};
+    /// # let _cleanup = unsafe { magnus::embed::init() };
+    ///
+    /// let regexp = RRegexp::new("b(.)r", Default::default()).unwrap();
+    /// regexp.reg_match("foo bar baz").unwrap();
+    ///
+    /// let match_data = backref_get().unwrap();
+    /// assert_eq!(match_data.post().to_string().unwrap(), " baz");
+    /// ```
     pub fn post(self) -> RString {
         unsafe { RString::from_rb_value_unchecked(rb_reg_match_post(self.as_rb_value())) }
     }
 
-    pub fn last(self) -> RString {
-        unsafe { RString::from_rb_value_unchecked(rb_reg_match_last(self.as_rb_value())) }
+    /// Returns the last capture.
+    ///
+    /// Returns `None` if there are no capture groups.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use magnus::{backref_get, RRegexp};
+    /// # let _cleanup = unsafe { magnus::embed::init() };
+    ///
+    /// let regexp = RRegexp::new("(.)oo b(.)r ba(.)", Default::default()).unwrap();
+    /// regexp.reg_match("foo bar baz").unwrap();
+    ///
+    /// let match_data = backref_get().unwrap();
+    /// assert_eq!(match_data.last().unwrap().to_string().unwrap(), "z");
+    /// ```
+    pub fn last(self) -> Option<RString> {
+        let value = unsafe { Value::new(rb_reg_match_last(self.as_rb_value())) };
+        (!value.is_nil()).then(|| unsafe { RString::from_rb_value_unchecked(value.as_rb_value()) })
     }
 }
 
