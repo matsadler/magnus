d85fbc3d6108619212aec657cf5528653bbd3784
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2023-04-12 11:13:46 -0400

    Make magnus::Error impl std::error::Error
    So it can be used with other error handling crates.

--- src/error.rs
+++ src/error.rs
@@ -198,6 +198,8 @@ impl Error {
     }
 }
 
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &self.0 {
